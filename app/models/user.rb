77c2df8647b192201c2f543023034879a95e2332
Willy <wwilimek@gmail.com>
2013-11-13 22:36:49 -0600

    Adjusting how users are found after finding out the token changes after each time a user logs in. Keying off the xid is much more suited for this kind of query.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
 
   def self.find_or_create_jawbone_user(token)
-    user = User.where(:up_access_token => token).first
-    puts "USER:#{user.inspect}"
+    jawbone_profile = JawboneUp.new.profile(token)
+    user = User.where(:up_user_xid => jawbone_profile.xid).first
     if user.blank?
-      jawbone_profile = JawboneUp.new.profile(token)
       unless jawbone_profile.blank?
 	User.create( :first_name      => jawbone_profile.first_name,
 		     :last_name       => jawbone_profile.last_name,
